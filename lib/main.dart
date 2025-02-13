57dc070bf700ab1328269a69359f4733b6942435
CodeAppBeefood <trongdv1907@gmail.com>
2025-02-13 08:32:18 +0700

    fix open cam

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,14 @@ import 'package:stargazer/features/camera/presentation/camera_page.dart';
 void main() {
   runApp(MaterialApp(home: CameraPage()));
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // ...
+    );
+  }
+}
